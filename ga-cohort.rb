53194487e212f74607754dca7d3de4ae05024c80
micah rich <micah@micahrich.com>
2015-06-17 00:07:42 -0700

    0.0.2 sha bump

--- ga-cohort.rb
+++ ga-cohort.rb
@@ -1,9 +1,9 @@
 class GaCohort < Formula
   desc "A tiny little app to get your GA Baseline-copied Trello ready to go"
   homepage "https://github.com/micahbrich/ga-cohort/"
-  url "https://github.com/micahbrich/ga-cohort/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "5ebea20f444af1c35087ad9d24c688167a697779bf38614e20aac032bccebfc1"
+  url "https://github.com/micahbrich/ga-cohort/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "e55afe2dcb7eb27806c33953fe4f4d677791c09a329638f72155f9309aa2dafd"
 
   def install
     bin.install "cohort"
