86b9315ec3df6724f1e227396efd1ce094cf6eeb
micah rich <micah@micahrich.com>
2015-06-30 21:34:15 -0700

    rename git-trello

--- git-trello.rb
+++ git-trello.rb
@@ -1,11 +1,9 @@
-class GaGitTrello < Formula
+class GitTrello < Formula
   desc "A tiny little git tool to do some standard setup for you when creating a new baseline resource"
-  homepage "https://github.com/micahbrich/ga-git-trello/"
-  url "https://github.com/micahbrich/ga-git-trello/archive/0.1.0.tar.gz"
+  homepage "https://github.com/micahbrich/git-trello/"
+  url "https://github.com/micahbrich/git-trello/archive/0.1.0.tar.gz"
   version "0.1.0"
   sha256 'f4cd5ff1ab88231cc14d73b7b4709bb43b183f07879c37609f96ce99847a08d9'
-
-  depends_on "ruby-trello" => :ruby
 
   def install
     bin.install "git-trello"
