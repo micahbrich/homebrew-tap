6be26e66439aa9b77698e7606b774ef0105aff00
micah rich <micah@micahrich.com>
2015-06-30 21:21:33 -0700

    update brew formula to define ruby-trello dependency

--- ga-git-trello.rb
+++ ga-git-trello.rb
@@ -3,11 +3,12 @@ class GaGitTrello < Formula
   homepage "https://github.com/micahbrich/ga-git-trello/"
   url "https://github.com/micahbrich/ga-git-trello/archive/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "f4cd5ff1ab88231cc14d73b7b4709bb43b183f07879c37609f96ce99847a08d9"
+  sha256 'f4cd5ff1ab88231cc14d73b7b4709bb43b183f07879c37609f96ce99847a08d9'
+
+  depends_on "ruby-trello" => :ruby
 
   def install
     bin.install "git-trello"
-    `gem install ruby-trello`
   end
 
 end
