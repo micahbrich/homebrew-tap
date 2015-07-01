fb8dfae23535ea07c5cf05d72e104051b3e33d83
micah rich <micah@micahrich.com>
2015-06-30 21:37:37 -0700

    fix sha git-trello

--- git-trello.rb
+++ git-trello.rb
@@ -3,7 +3,7 @@ class GitTrello < Formula
   homepage "https://github.com/micahbrich/git-trello/"
   url "https://github.com/micahbrich/git-trello/archive/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 'f4cd5ff1ab88231cc14d73b7b4709bb43b183f07879c37609f96ce99847a08d9'
+  sha256 'f685fbb1f75b44c484c32fcc4a933f0bd8f8b1a9add2fde5dc4dd93d65532336'
 
   def install
     bin.install "git-trello"
