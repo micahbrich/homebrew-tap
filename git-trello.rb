20fe93d48720503238f13f32b77305c505e3d2a1
micah rich <micah@micahrich.com>
2015-06-30 23:24:04 -0700

    bump git trello 0.2.0

--- git-trello.rb
+++ git-trello.rb
@@ -1,9 +1,9 @@
 class GitTrello < Formula
   desc "A tiny little git tool to do some standard setup for you when creating a new baseline resource"
   homepage "https://github.com/micahbrich/git-trello/"
-  url "https://github.com/micahbrich/git-trello/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 'f685fbb1f75b44c484c32fcc4a933f0bd8f8b1a9add2fde5dc4dd93d65532336'
+  url "https://github.com/micahbrich/git-trello/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 'b49611cee2c4e48f3fefc4820c37fd14174fc347e2832af81e860ec7e886fa3e'
 
   def install
     bin.install "git-trello"
